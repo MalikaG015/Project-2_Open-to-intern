06a78c2b17697d674ef05f05099b69dcbc897dff
MalikaG015 <malikagulati015@gmail.com>
2022-02-09 01:20:07 +0530

    project-2/open-to-intern

---
+++ src/routes/route.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const collegeController = require('../controllers/collegeController')
+const internsController = require('../controllers/internsController')
+
+
+//1)for saving college details
+router.post('/functionUp/Colleges', collegeController.createCollege)
+
+//2) for saving intern setails
+router.post('/functionUp/interns', internsController.createInterns)
+
+
+//3)return list of all interns applied for that college
+router.get('/functionup/collegeDetails',internsController.giveAllInterns)
+
+
+module.exports = router;
